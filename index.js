635c12b2ab8f8962c5f39e1551148c9498ae6f05
Manju Thomas <kudukacar@hotmail.com>
2018-08-29 16:53:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key]:value});
 }
 function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
-  Object[key] = value;
-  return Object;
+  obj[key] = value;
+  return obj;
 }
 function deleteFromObjectByKey(obj, key) {
   var newObj = Object.assign({}, obj)
